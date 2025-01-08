830f66a0a6d1be17b853e0517b54712c86836363
Raghu K <raghuk.vit@gmail.com>
2025-01-08 06:52:56 +0530

    RoboSHop Project

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -3,6 +3,7 @@ resource "azurerm_kubernetes_cluster" "main" {
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
   kubernetes_version  = "1.31.2"
+  dns_prefix          = "dev"
 
   default_node_pool {
     name       = "default"
