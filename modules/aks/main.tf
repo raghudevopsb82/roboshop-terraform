1afe998e7bf1fd2059f47984715be670809ea2e3
Raghu K <raghuk.vit@gmail.com>
2025-01-27 05:48:40 +0530

    RoboSHop Project

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -9,9 +9,6 @@ resource "azurerm_kubernetes_cluster" "main" {
     name       = "default"
     node_count = 1
     vm_size    = "Standard_D2_v2"
-    auto_scaling_enabled = true
-    min_count = 1
-    max_count = 10
   }
 
 
