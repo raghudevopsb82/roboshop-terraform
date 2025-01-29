76129318353308ca7d3ac1180468272f3c3f7a60
Raghu K <raghuk.vit@gmail.com>
2025-01-29 06:58:03 +0530

    RoboSHop Project

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -12,6 +12,7 @@ resource "azurerm_kubernetes_cluster" "main" {
     auto_scaling_enabled = true
     min_count            = 1
     max_count            = 10
+    pod_subnet_id = "/subscriptions/7b6c642c-6e46-418f-b715-e01b2f871413/resourceGroups/project-setup-1/providers/Microsoft.Network/virtualNetworks/project-setup-network/subnets/default"
   }
 
   aci_connector_linux {
