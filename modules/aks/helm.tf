69e4e4b4502d8edf9e0d8f25bad7fde101e4ee42
SVR Kalluru <raghuk.vit@gmail.com>
2025-02-03 06:55:05 +0530

    RoboShop

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -1,4 +1,5 @@
 resource "null_resource" "kubeconfig" {
+  depends_on = [azurerm_kubernetes_cluster.main]
   provisioner "local-exec" {
     command = <<EOF
 az aks get-credentials --resource-group ${data.azurerm_resource_group.main.name} --name main --overwrite-existing
@@ -9,6 +10,7 @@ EOF
 
 
 resource "helm_release" "external-secrets" {
+  depends_on = [null_resource.kubeconfig]
   name       = "external-secrets"
   repository = "https://charts.external-secrets.io"
   chart      = "external-secrets"
