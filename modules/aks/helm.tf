479fb83b51949dec761382493094414b01f39d72
Raghu K <raghuk.vit@gmail.com>
2025-02-12 06:59:05 +0530

    RoboSHop Project

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -20,7 +20,7 @@ resource "null_resource" "external-secrets" {
   depends_on = [helm_release.external-secrets]
   provisioner "local-exec" {
     command = <<EOF
-kubectl create secret vault-token --from-literal=token=${var.vault_token}
+kubectl create secret generic vault-token --from-literal=token=${var.vault_token}
 EOF
   }
 }
