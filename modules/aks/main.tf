12d71f9173f2518d63aa4a42b96c5d5b07ef5c7a
Raghu K <raghuk.vit@gmail.com>
2025-02-14 05:44:35 +0530

    RoboSHop Project

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -7,10 +7,10 @@ resource "azurerm_kubernetes_cluster" "main" {
 
   default_node_pool {
     name                 = "default"
-    node_count           = 1
+    node_count           = 2
     vm_size              = "Standard_D2_v2"
     auto_scaling_enabled = true
-    min_count            = 1
+    min_count            = 2
     max_count            = 10
     #pod_subnet_id = "/subscriptions/7b6c642c-6e46-418f-b715-e01b2f871413/resourceGroups/project-setup-1/providers/Microsoft.Network/virtualNetworks/project-setup-network/subnets/default"
     vnet_subnet_id = "/subscriptions/7b6c642c-6e46-418f-b715-e01b2f871413/resourceGroups/project-setup-1/providers/Microsoft.Network/virtualNetworks/project-setup-network/subnets/default"
