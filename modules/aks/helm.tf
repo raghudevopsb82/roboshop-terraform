92cf40cdc9bf7971dda49dc2b34ce290e4ef1add
Raghu K <raghuk.vit@gmail.com>
2025-03-10 07:14:42 +0530

    RoboSHop Project

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -142,3 +142,15 @@ resource "helm_release" "external-dns" {
 }
 
 
+resource "helm_release" "filebeat" {
+  depends_on = [null_resource.kubeconfig]
+  name       = "filebeat"
+  repository = "https://helm.elastic.co"
+  chart      = "filebeat"
+  namespace  = "kube-system"
+  values = [
+    file("${path.module}/files/filebeat.yaml")
+  ]
+}
+
+
