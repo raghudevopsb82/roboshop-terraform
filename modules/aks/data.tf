f99d2e32d4ec8d69775ce3bb88c9ce6efebdf6d9
SVR Kalluru <raghuk.vit@gmail.com>
2025-03-16 11:43:34 +0530

    RoboShop

--- modules/aks/data.tf
+++ modules/aks/data.tf
@@ -1,9 +1,14 @@
 data "azurerm_container_registry" "main" {
   name                = "roboshopb82new"
-  resource_group_name = "project-setup-1"
+  resource_group_name = data.azurerm_resource_group.default.name
 }
 
 data "vault_generic_secret" "az" {
   path = "infra/github-actions"
+}
+
+data "azurerm_subscription" "current" {}
+data "azurerm_resource_group" "default" {
+  name = "project-setup-1"
 }
 
--- variables.tf
+++ variables.tf
@@ -1,10 +1,7 @@
 variable "env" {}
 variable "token" {}
-#variable "components" {}
 variable "databases" {}
-variable "subscription_id" {
-  default = "a906d619-0839-4738-a908-227a8b69d458"
-}
+variable "subscription_id" {}
 variable "aks" {}
 variable "resource_group" {}
 variable "vnet" {}
