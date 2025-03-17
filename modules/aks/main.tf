a05092ce53eef3242d2df65bd6793316b1127ae6
SVR Kalluru <raghuk.vit@gmail.com>
2025-03-17 06:50:04 +0530

    RoboShop

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_kubernetes_cluster" "main" {
     name                 = "default"
     node_count           = 2
     vm_size              = "Standard_D4_v2"
-    auto_scaling_enabled = true
+    auto_scaling_enabled = false
     min_count            = 2
     max_count            = 10
     #pod_subnet_id = "/subscriptions/7b6c642c-6e46-418f-b715-e01b2f871413/resourceGroups/project-setup-1/providers/Microsoft.Network/virtualNetworks/project-setup-network/subnets/default"
