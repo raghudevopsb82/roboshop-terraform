8b3a630881fdca71f29707d6f01d5e2e711983da
SVR Kalluru <raghuk.vit@gmail.com>
2025-03-20 06:44:44 +0530

    RoboShop

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -14,7 +14,6 @@ resource "azurerm_subnet" "main" {
   count               = length(var.subnets)
   name                 = "${var.rg_name}-vnet-subnet-${count.index+1}"
   virtual_network_name = azurerm_virtual_network.main.name
-  location            = var.rg_location
   resource_group_name = var.rg_name
   address_prefixes     = var.subnets[count.index]
 }
