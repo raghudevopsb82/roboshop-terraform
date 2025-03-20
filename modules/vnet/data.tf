466cd17cd9e957b88f965fc18e23a50591d29101
SVR Kalluru <raghuk.vit@gmail.com>
2025-03-20 07:13:57 +0530

    RoboShop

--- modules/vnet/data.tf
+++ modules/vnet/data.tf
@@ -2,3 +2,8 @@ data "azurerm_resource_group" "project" {
   name = "project-setup-1"
 }
 
+data "azurerm_virtual_network" "project" {
+  name                = "main"
+  resource_group_name = data.azurerm_resource_group.project.name
+}
+
--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -21,13 +21,13 @@ resource "azurerm_virtual_network_peering" "main-to-project" {
   name                      = "main-to-project"
   resource_group_name       = var.rg_name
   virtual_network_name      = azurerm_virtual_network.main.name
-  remote_virtual_network_id = "default"
+  remote_virtual_network_id = data.azurerm_virtual_network.project.name
 }
 
 resource "azurerm_virtual_network_peering" "project-to-mai" {
   name                      = "project-to-main"
   resource_group_name       = data.azurerm_resource_group.project.name
-  virtual_network_name      = "default"
+  virtual_network_name      = data.azurerm_virtual_network.project.name
   remote_virtual_network_id = azurerm_virtual_network.main.id
 }
 
